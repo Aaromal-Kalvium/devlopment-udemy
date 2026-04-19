cc61efa811187a44aa6db30830a94ab24c0dcd97
Aaromal <aaromalps56@gmail.com>
2026-04-19 21:05:58 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import CourseDirectory from './components/CourseDirectory'
+
+function App() {
+  return (
+    <div className="App">
+      {/* Just your component! The main project will wrap this in their Navbar/Footer */}
+      <CourseDirectory />
+    </div>
+  )
+}
+
+export default App
